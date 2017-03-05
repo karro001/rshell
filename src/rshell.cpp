b7c300f42a0f078dc27aed751aef477de57997a6
karro001 <karro001@ucr.edu>
2017-03-04 21:55:54 -0800

    Add files via upload

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -15,135 +15,215 @@
 #include <vector>
 #include <map>
 using namespace std;
-
-void cmd(const string &line,string delim);
+bool balancedPrecedence(string line);
+void findDelim(string &cpy, string &delim);
+void cmd(const string &line,string delim, int &success);
 int exec(const string &line);
+int Test(const string line);
 
 int main()
 {
-
-  
+  string delim;
+  int success = 0;
   while (1)
   {
+    input:
     cout << getcwd(NULL, 0) << " $ " << flush; //couts the current working directory
     
     string line;
     string temp;
     getline(cin, line);
+    findDelim(line,delim);
+    // delim.clear();
+  
     
+    begin:
     if(line.find("#") != string::npos)
     {
-      if(line.find("#") != 0){
-        line = line.substr(0,line.find("#"));
+      if(line.find("#") == 0){
+        goto input;
       }
       else{
-        line.clear();
+        line = line.substr(0,line.find("#"));
       }
     }
     if(line == "exit")
       return 0;
-      
-    else if(line.find("&&") != string::npos){
-      cmd(line,"&&");
-    }
-    else if(line.find("||") != string::npos){
-      cmd(line,"||");
-    }
-    else if(line.find(";") != string::npos){
-      cmd(line,";");
-    }
-    else if(line.size() != 0)
-      exec(line);
+    
+    if(delim == "("){
+      if(!balancedPrecedence(line)) 
+        goto input;
+      if(balancedPrecedence(line)){
+        temp = line.substr(line.find_last_of("(") + 1, line.find(")") - 1);
+        findDelim(temp,delim);
+        // cout << "TEMP: " << temp << endl;
+        if(delim.size() == 0)
+          exec(temp);
+        else{
+          cmd(temp,delim,success);
+        }
         
+        
+        // temp = line.substr(line.find(")") + 1, line.size());
+        temp = line.erase(line.find_last_of("("), line.find(")")+1);
+        line = temp;
+        // cout << "TEMP2: " << temp << endl; //&& echo hi
+        again: 
+        findDelim(line,delim);
+        if(delim.size() != 0)
+        {
+          // cout << success << endl;
+          temp = line.substr(line.find(delim) + delim.size(), line.size());
+          line = temp; //echo hi, with delim = &&, success != 0
+          // cout << "TEMP: " << temp << endl;
+          if(delim == "&&" && success == 0)
+            goto begin;
+          else if(delim == "&&" && success != 0)
+            goto again;
+          else if(delim == "||" && success == 0)
+            goto again;
+          else if(delim == "||" && success != 0)
+            goto begin;
+          else
+            goto begin;
+        }
+        else{
+          goto input;
+        }
+      }
+    }
+    else{
+      if(delim.size() != 0)
+        cmd(line,delim,success);
+      else if(line.size() != 0){
+        // cout << "TEMP: " << temp << endl;
+        // cout << "Entered exec" << endl << "Delim: " << delim << endl;
+        exec(line);
+      }
+    }
   }
   return 0;
 }
-
-void cmd(const string &line,string delim){
-  
-    // const char *line1 = line.c_str();
-    int success = 0;
-    int counter = 0;
+void cmd(const string &line,string delim, int& success){
+    //cout << "LINE: " << line << endl;
     string cpy = line;
+    string tempDelim = delim;
+    string copyDelim = delim;
+    
+    findDelim(cpy,copyDelim);
+    if(copyDelim.size() == 0){
+      exec(line);
+      return;
+    }
+    copyDelim = delim;
     string temp = cpy.substr(0,cpy.find(delim));
-    while(cpy.find(delim) != string::npos || counter < 2){
-      // int child = fork();
-      // if(child < 0){
-      //   perror("Fork failed");
-      //   exit(1);
-      // }
-      // else if(child == 0){
-      success = exec(temp);
-      // cout << "SUCCESS" << success << endl;
-      //   exit(0);
-      // }
-      // else{
-      //   if ( waitpid( child, 0, 0 ) < 0 ){
-      //     perror("Parent wait failed");
-      //     exit(1);
-      //   }
-      // }
-      if(cpy.find(delim) != string::npos){
-        temp = cpy.substr(cpy.find(delim)+delim.size(),cpy.size());
-        cpy = temp;
-        temp.clear();
-      }
-      if(delim == "&&" && success == 0){
-        if(cpy.find(delim) != string::npos){
-          temp = cpy.substr(0,cpy.find(delim));
-        }
-        else{
-          temp = cpy.substr(0,cpy.size());
-          counter++;
-        }
-      }
-      else if(delim == "&&" && success == -1){
-        return;
-      }
-      if(delim == ";"){
-        if(cpy.find(delim) != string::npos){
-          temp = cpy.substr(0,cpy.find(delim));
-        }
-        else{
-          temp = cpy.substr(0,cpy.size());
-          counter++;
-        }
-      }
-      if(delim == "||" && success == -1){
-        if(cpy.find(delim) != string::npos){
-          temp = cpy.substr(0,cpy.find(delim));
-        }
-        else{
-          temp = cpy.substr(0,cpy.size());
-          counter++;
-        }
-      }
-      else if(delim == "||" && success != -1){
-        temp.clear();
-        return;
-      }
-      // else if(delim == ";"){
-      //   if(cpy.find(delim) != string::npos)
-      //     temp = cpy.substr(0,cpy.find(delim));
-        // else
-        //   temp = cpy.substr(0,cpy.size());
-      // }
-  }
+    while(1){
       
+      tempDelim = copyDelim;
+      success = exec(temp);
+      if(temp == cpy)
+        return;
+      if(cpy.find(tempDelim) != string::npos){
+        temp = cpy.substr(cpy.find(tempDelim)+tempDelim.size(),cpy.size());
+        cpy = temp;
+        temp.clear();                              
+      }
+      if(tempDelim == "&&" && success == 0){
+        findDelim(cpy,copyDelim);
+        if(cpy.find(copyDelim) != string::npos && copyDelim.size() != 0){
+          temp = cpy.substr(0,cpy.find(copyDelim));
+        }
+        else{
+          temp = cpy;
+        }
+      }
+      else if(tempDelim == "&&" && success == -1){
+        copyDelim = tempDelim;
+        while(copyDelim == "&&" && copyDelim.size() != 0){
+          findDelim(cpy,copyDelim);
+          temp = cpy.substr(cpy.find(copyDelim) + copyDelim.size(),cpy.size());
+          cpy = temp;
+        }
+        if(copyDelim.size() == 0){
+          return;
+        }
+      }
+      if(tempDelim == ";"){
+        findDelim(cpy,copyDelim);
+        if(cpy.find(copyDelim) != string::npos && copyDelim.size() != 0){
+          temp = cpy.substr(0,cpy.find(copyDelim));
+        }
+        else{
+          temp = cpy;
+        }
+      }
+      if(tempDelim == "||" && success == 0){
+        findDelim(cpy,copyDelim);
+        if(cpy.find(copyDelim) != string::npos && copyDelim.size() != 0){
+          temp = cpy.substr(cpy.find(copyDelim) + copyDelim.size(),cpy.size());
+          cpy = temp;
+        }
+        else{
+          return;
+        }
+      }
+      else if(tempDelim == "||" && success == -1){
+        findDelim(cpy,copyDelim);
+        // cout << "CPY: " << cpy << endl;
+        if(cpy.find(copyDelim) != string::npos && copyDelim.size() != 0){
+          temp = cpy.substr(0,cpy.find(copyDelim));
+        }
+        else{
+          temp = cpy;
+        }
+      }
+      if(tempDelim == "("){
+        
+        temp = cpy.substr(cpy.find_last_of("(")+1, cpy.find(")") - 1);
+        
+        
+        
+        
+  
+        findDelim(cpy,copyDelim);
+        if(cpy.find(copyDelim) != string::npos && copyDelim.size() != 0){
+          temp = cpy.substr(0,cpy.find(copyDelim));
+        }
+      }
+    }
 }
 int exec(const string &line){
-    
-
+    //cout << "Line: " << line << endl;
+    if((line.find("test") == 0 || line.find("[") == 0) || (line.find("test") == 1 || line.find("[") == 1))
+    {
+      if((line.find("test") != string::npos || line.find("[ ") != string::npos) && line.find("/") != string::npos){
+          // brackets = true;
+          // if(balancedPrecedence(line))
+          //   return -1;
+          if(Test(line) == 0)
+            return 0;
+          else
+            return -1;
+      }
+      else if((line.find("test") != string::npos && line.find("/") == string::npos) || (line.find("[") != string::npos && line.find("/") == string::npos)){
+        cout << "Failed to execute: No such file or directory" << endl;
+        return -1;
+      }
+    }
     const char* temp = line.c_str();
     char* lineCopy = (char*)temp;
     vector<char*> args;
     char* prog = strtok(lineCopy, " " );
     string test = prog;
+    // cout << "TEST: " << test << endl;
     if(test == "exit"){
       exit(0);
     }
     char* tmp = prog;
+    
     pid_t child = fork();
+    
+  
     if(child < 0){
       perror("FORK FAILED");
       exit(1);
@@ -161,42 +241,170 @@ int exec(const string &line){
       
       argv[args.size()] = NULL;
       if (execvp(prog, argv) < 0){
-          // kill(child, SIGKILL);
-          // kill(child, SIGTERM);
           perror("Failed to execute");
           exit(1);
   
       }
       else{
-        // kill(child, SIGKILL);
-        // kill(child, SIGTERM);
         exit(2);
       }
     }
     else{
-        // if ( waitpid( child, 0, 0 ) < 0 ){
-          // perror("Parent wait failed");
-        //   exit(1);
-        // }
         int status;
         waitpid(child, &status, WUNTRACED|WCONTINUED);
         
-        // cout << WEXITSTATUS(status) << endl;
         if(WEXITSTATUS(status) == 0){
           return 0;
         }
         else{
-           kill(child,SIGUSR1);
+          kill(child,SIGUSR1);
           return -1;
         }
-        // if(status != 0)
-        //   return 0;
-        // else
-        //   return -1;
-        // if ( WIFEXITED(status) == 0)
-        //   return 0;
-        // else
-        //   return -1;
     }
+    exit(1);
     return 0;
+}
+void findDelim(string &cpy, string &delim){
+      unsigned pos = 4096;
+      // cout << cpy << endl;
+      delim.clear();
+      if(cpy.find("&&") != string::npos){
+        // cout << "ENTERED: &&" << endl;
+        delim = "&&";
+        pos = cpy.find("&&");
+      }
+      if(cpy.find("||") != string::npos && cpy.find("||") < pos){
+        // cout << "ENTERED: ||" << endl;
+        delim = "||";
+        pos = cpy.find("||");
+        // cout << pos << endl;
+      }
+      if(cpy.find(";") != string::npos && cpy.find(";") < pos){
+        // cout << "ENTERED: ;" << endl;
+        delim = ";";
+        pos = cpy.find(";");
+      }
+      if(cpy.find("(") != string::npos && cpy.find("(") < pos){
+        // cout << "ENTERED: ;" << endl;
+        //if(balancedPrecedence(cpy)){
+          delim = "(";
+          pos = cpy.find("(");
+        //}
+      }
+}
+bool balancedPrecedence(string line)
+{
+    int par_count = 0;
+    int brac_count = 0;
+    for(unsigned i = 0; i < line.size(); ++i)
+    {
+        if(line.at(i) == '(')
+            ++par_count;
+        else if(line.at(i) == ')')
+            --par_count;
+        else if(line.at(i) == '[')
+            ++brac_count;
+        else if(line.at(i) == ']')
+            --brac_count;
+    }
+    if(par_count == 0 && brac_count == 0)
+        return true;
+    else{
+      if(par_count != 0 && brac_count != 0){
+        cout << "Unbalanced number of parentheses or brackets" << endl;
+        return false;
+      }
+      else if(par_count != 0){
+        cout << "Unbalanced number of parentheses" << endl;
+        return false;
+      }
+      else{
+        cout << "Unbalanced number of brackets" << endl;
+        return false;
+      }
+    }
+}
+
+int Test(const string line)
+{
+    string copy = line;
+    string flag;
+    string temp;
+    string delim;
+    int success;
+    int flagtype = 1; // 1 = -e, 2 = -d, 3 = -f
+        
+    if(copy.find("test") != string::npos){
+        // cout << "COPY1: " << copy << endl; 
+        // cout << "COPY1: " << copy << endl;
+        if(copy.find('-') != string::npos)
+          flag = copy.substr(copy.find("-"),copy.find("-") +1);
+        // cout << "FLAG: " << flag << endl;
+        if(copy.find("/") != string::npos)
+          copy = copy.substr(copy.find("/") + 1,copy.size());
+        if(copy.find(' ')){
+          copy = copy.substr(0,copy.find(' '));
+          // cout << "COPY1: " << copy << endl;
+          // temp = copy.substr(0, copy.find(' '));
+          // copy = copy.substr(copy.find(' ') + 1, copy.size());
+          // findDelim(copy,delim);
+          // cout << "COPY2: " << copy << endl;
+          // if(delim.size() != 0)
+          //   copy = copy.substr(copy.find(delim) + delim.size(), copy.size());
+          // cout << "COPY3: " << copy << endl;
+        }
+    }
+    else if(copy.find("[ ") != string::npos){
+        if(copy.find('-') != string::npos)
+          flag = copy.substr(copy.find("-"),copy.find("-") +1);
+        if(copy.find("/") != string::npos){
+          copy = copy.substr(copy.find('/') + 1);
+          copy.erase(copy.find(" ]"));
+        }
+        else
+          return -1;
+    }
+
+    if(flag.find("-d") != string::npos)
+        flagtype = 2;
+    else if (flag.find("-f") != string::npos)
+        flagtype = 3;
+        
+    struct stat buffer;
+    vector<const char*> path;
+    path.push_back(copy.c_str());
+    
+    if(stat(const_cast<char*>(path[0]), &buffer) < 0 && flagtype == 1){
+        perror("Stat");
+        cout << "(False)" << endl;
+        success = -1;
+        if(delim == "||" || delim == ";")
+          cmd(copy,delim,success);
+        return 0;
+    }
+      if(flagtype == 1){
+          cout << "(True)" << endl;
+          if(delim == "&&" || delim == ";")
+            cmd(copy,delim,success);
+          return 0;
+      }   
+      else if(flagtype == 3){//check if path leads to a file
+          if(S_ISREG(buffer.st_mode))
+              cout << "(TRUE)" << endl;
+          else
+              cout << "(FALSE)" << endl;
+          return 0;
+      }   
+      else if(flagtype == 2){ // check if path leads to a directory
+          if(S_ISDIR(buffer.st_mode))
+              cout << "(TRUE)" << endl;
+          else
+              cout << "(FALSE)" << endl;
+          return 0;
+      }
+      else{
+          perror("Incorrect input for test");
+          return -1;
+      }
+    
 }
